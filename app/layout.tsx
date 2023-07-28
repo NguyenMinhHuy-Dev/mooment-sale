32374a5ea61d2874beaeb00c548c462209f22e13
minhhuy212 <nguyenminhhuy.0938745593@gmail.com>
2023-07-29 00:23:57 +0700

    commit 4

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,9 +17,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className="relative bg-white">
-        {/* <Navbar /> */}
+        <Navbar />
         {children}
-        {/* <TawkTo /> */}
+        <TawkTo />
       </body>
     </html>
   )
