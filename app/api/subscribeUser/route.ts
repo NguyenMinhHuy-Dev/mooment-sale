99dfb13b393028dcfdbacce541c5dd50b21ad5a3
minhhuy212 <nguyenminhhuy.0938745593@gmail.com>
2023-08-04 00:42:18 +0700

    subscribe form

---
+++ app/api/subscribeUser/route.ts
@@ -0,0 +1,65 @@
+// import fetch from 'isomorphic-unfetch';
+import { NextResponse } from 'next/server'
+
+export async function POST(request: Request) {  
+    const { email } = await request.json();
+
+    if (!email) {
+        return NextResponse.json({
+            error: "Email is required"
+        }, {
+            status: 400
+        })
+    }
+
+    try { 
+        const AUDIENCE_ID = process.env.NEXT_PUBLIC_MAILCHIMP_AUDIENCE_ID;
+        const API_KEY = process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY;
+        const DATACENTER = process.env.NEXT_PUBLIC_MAILCHIMP_API_SERVER;
+        const data = {
+            email_address: email,
+            status: 'subscribed',
+        };
+        // const response = await fetch(`https://${DATACENTER}.api.mailchimp/3.0/lists/${AUDIENCE_ID}/members`, {
+        //     body: JSON.stringify(data),
+        //     headers: {
+        //         Authorization: `apikey ${API_KEY}`,
+        //         'Content-Type': 'application/json',
+        //     },
+        //     method: 'POST',
+        // });
+ 
+        const url = process.env.NEXT_PUBLIC_MAILCHIMP_URL;
+        const response = await fetch(`${url}&EMAIL=${email}`, {
+            body: JSON.stringify({
+                email
+            }),
+    
+            headers: {
+            'Content-Type': 'application/json',
+            },
+    
+            method: 'POST',
+        })
+
+        if (response.status >= 400) {
+            return NextResponse.json({
+                error: "There was an error subscribing to the newsletter. Hit us up mooment.marketing@gmail.com and we'll add you the old fashioned way :(."
+            }, {
+                status: 400
+            });
+        }
+        return NextResponse.json({
+            error: '',
+        }, {
+            status: 201,
+        })
+    }
+    catch (err: any) { 
+        return NextResponse.json({
+            error: err.message || err.toString(),
+        }, {
+            status: 500,
+        })
+    } 
+};
--- components/SubscribeFormSection.tsx
+++ components/SubscribeFormSection.tsx
@@ -7,55 +7,48 @@ export default function SubscribeFormSection() {
 
     const onSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        setLoading(true);
-        const url = process.env.NEXT_PUBLIC_MAILCHIMP_URL;
-        await fetch(`${url}&EMAIL=${email}`, {
+        
+        const res = await fetch('/api/subscribeUser', {
             body: JSON.stringify({
-              email
+                email: email,
             }),
-      
+
             headers: {
-              'Content-Type': 'application/json',
+                'Content-Type': 'application/json',
             },
-      
+
             method: 'POST',
-          })
+        });
         // await jsonp(`${url}&EMAIL=${email}`, { param: 'c' }, (_: any, data: any) => {
         //     const { msg, result } = data
             
         //     setLoading(false);
         //     setMsg("Cảm ơn vì bạn đã đăng ký!");
         // }); 
+
     };
 
-  return (
-    <div className='mygrid SubscribeForm p-4 flex items-center mb-[50px] justify-center'> 
-        {loading ? (
-            <span className='loading-submit'>Đang đăng ký...</span>
-        ) : (
-            <form onSubmit={onSubmit}>
-                <div className='SubscribeForm-container'>
-                    <div className='SubscribeForm-container-left'>
-                        <h2 className='font-bold text-black'>Đăng ký để nhận những khuyến mãi tốt nhất</h2> 
-                        <div className="mygroup"> 
-                            {message ? (
-                                <span className='subscribe-msg'>{message}</span>
-                            ) : (
-                                
-                                <input type="email" name="EMAIL" placeholder='Nhập email của bạn vào đây' className="required email" id="mce-EMAIL" required value={email} onChange={e => setEmail(e.target.value)} /> 
-                            
-                            )}
-                        </div>
-                    </div>
-                    
-                    <div className="SubscribeForm-container-right">
-                        <div className="clear foot">
-                            <input type="submit" name="subscribe" id="mc-embedded-subscribe" className="button" value="Đăng ký nhận tin"/> 
-                        </div>
-                    </div>
-                </div>
-            </form> 
-        )}
-    </div>
-  )
+    return (
+        <div className='mygrid p-4 flex items-center flex-col mb-[50px] justify-center'> 
+            {loading ? (
+                <span className='loading-submit'>Đang đăng ký...</span>
+            ) : (
+                <>
+                    <h2 className='mytitlewithtextshadow font-black text-[40px] '>Mua sắm online thả ga với vô vàn ưu đãi tại Mooment</h2>
+                    <form onSubmit={onSubmit} className='shadow-xl mt-4 w-full h-[100px] p-6 flex items-center rounded-[10px] mygradientbackground'>
+                        <input
+                            placeholder='Đăng ký nhận ưu đãi với email'
+                            type='email'
+                            name='email'
+                            value={email}
+                            onChange={(e) => setEmail(e.target.value)}
+                            // required
+                            className='flex-1 full px-2 py-4 outline-none border-2 border-[#464646] text-[16px] text-white bg-[#464646] rounded focus:border-[#000]'
+                        />
+                        <button type='submit' className=' inline-block px-4 py-4 text-[16px] uppercase bg-light-gray text-white font-bold  ml-4 rounded transition-all hover:bg-white hover:text-black'>Đăng ký</button>
+                    </form>
+                </>
+            )}
+        </div>
+    )
 }
