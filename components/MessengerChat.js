cd38ed5266b873b6d232f4c7ef5737145c9cac28
minhhuy212 <nguyenminhhuy.0938745593@gmail.com>
2023-08-20 04:21:34 +0700

    plugin mess #3

--- components/MessengerChat.js
+++ components/MessengerChat.js
@@ -2,27 +2,27 @@
 import React, { useEffect } from 'react';
 
 export default function MessengerChat() { 
-    useEffect(() => {
-      
-    var chatbox = document.getElementById('fb-customer-chat')
-    chatbox?.setAttribute("page_id", process.env.NEXT_PUBLIC_FB_PAGE_ID);
-    chatbox?.setAttribute("attribution", "biz_inbox");
+    useEffect(() => { 
 
-    window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v17.0'
-        });
-      };
+        var chatbox = document.getElementById('fb-customer-chat')
+        chatbox?.setAttribute("page_id", "108759702288856");
+        chatbox?.setAttribute("attribution", "biz_inbox");
 
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-        
+        window.fbAsyncInit = function() {
+            FB.init({
+            xfbml            : true,
+            version          : 'v17.0'
+            });
+        };
+
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = 'https://connect.facebook.net/vi_VN/sdk/xfbml.customerchat.js';
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
+            
     })
     return (
         <> 
