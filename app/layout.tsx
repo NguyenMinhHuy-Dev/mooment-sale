8113623efe03f7927f1b2223ba2497a6e8c23167
minhhuy212 <nguyenminhhuy.0938745593@gmail.com>
2023-08-20 15:10:55 +0700

    mailchimp popup #3

--- app/layout.tsx
+++ app/layout.tsx
@@ -48,7 +48,7 @@ export default function RootLayout({
             <Footer />
           </Providers> 
       </body>
-      <Script id="mcjs" strategy="beforeInteractive">
+      <Script id="mcjs">
             {`!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/b20d117330404a90801147d3f/1d24ba73d202403817eda5d3d.js");`}
       </Script>
     </html>
